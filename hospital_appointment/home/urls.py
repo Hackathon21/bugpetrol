920ccf190cfb16f359049c10c5b2ecdacd583fec
amrit5s2002 <77382202+amrit5s2002@users.noreply.github.com>
2021-06-25 12:48:17 +0530

    reverting

--- hospital_appointment/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django import urls
-from django.conf.urls import include
-from django.urls import path
-from home.views import *
-
-
-urlpatterns = [
-     path('',homepage, name = 'homepage'),
-]
